e5ac2deb3d331842926d91ca7f3af68cf8569269
Stephan Hoyer <ste.hoyer@gmail.com>
2012-12-11 18:49:18 +0100

    #2 Add test for chuk main code

---
+++ test/index.js
@@ -0,0 +1,38 @@
+var chuk = require('../')
+  , repl
+  , config = {
+  'foo': function(context) {
+    context.foo = 'baz';
+  },
+  'bar': function(context) {
+    context.bar = 123;
+  },
+  'default': function(context) {
+    context.foo = 'default';
+  }
+}
+describe('chuk', function() {
+  it('should load chukfile configuration if it\'s a function', function() {
+    repl = chuk(null, function(context) {
+      context.foo = 'bar';
+    });
+    repl.context.global.should.have.property('foo');
+    repl.context.global.foo.should.equal('bar');
+  });
+  it('should load chukfile configuration for given env', function() {
+    repl = chuk('foo', config);
+    repl.context.global.should.have.property('foo');
+    repl.context.global.should.not.have.property('bar');
+    repl.context.global.foo.should.equal('baz');
+    repl = chuk('bar', config);
+    repl.context.global.should.not.have.property('foo');
+    repl.context.global.should.have.property('bar');
+    repl.context.global.bar.should.equal(123);
+  });
+  it('should load default configuration if no env given', function() {
+    repl = chuk(null, config);
+    repl.context.global.should.have.property('foo');
+    repl.context.global.should.not.have.property('bar');
+    repl.context.global.foo.should.equal('default');
+  });
+});
