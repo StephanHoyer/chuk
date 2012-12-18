14b516be7c378d23b8c167743d50121f7880f80d
Stephan Hoyer <ste.hoyer@gmail.com>
2012-12-18 10:57:06 +0100

    Change name of repl object to solve travis build problem

--- test/index.js
+++ test/index.js
@@ -20,14 +20,14 @@ describe('chuk', function() {
     repl.context.global.foo.should.equal('bar');
   });
   it('should load chukfile configuration for given env', function() {
-    var repl = chuk('foo', config);
-    repl.context.global.should.have.property('foo');
-    repl.context.global.should.not.have.property('bar');
-    repl.context.global.foo.should.equal('baz');
-    repl = chuk('bar', config);
-    repl.context.global.should.not.have.property('foo');
-    repl.context.global.should.have.property('bar');
-    repl.context.global.bar.should.equal(123);
+    var repl1 = chuk('foo', config);
+    repl1.context.global.should.have.property('foo');
+    repl1.context.global.should.not.have.property('bar');
+    repl1.context.global.foo.should.equal('baz');
+    var repl2 = chuk('bar', config);
+    repl2.context.global.should.not.have.property('foo');
+    repl2.context.global.should.have.property('bar');
+    repl2.context.global.bar.should.equal(123);
   });
   it('should load default configuration if no env given', function() {
     var repl = chuk(null, config);
