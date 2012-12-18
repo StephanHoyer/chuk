41ca332ad293b9513e6ae7286497d53dcc4b750e
Stephan Hoyer <ste.hoyer@gmail.com>
2012-12-18 10:55:08 +0100

    Change scope of repl object to solve travis build problem

--- test/index.js
+++ test/index.js
@@ -1,5 +1,4 @@
 var chuk = require('../')
-  , repl
   , config = {
   'foo': function(context) {
     context.foo = 'baz';
@@ -10,17 +9,18 @@ var chuk = require('../')
   'default': function(context) {
     context.foo = 'default';
   }
-}
+};
+
 describe('chuk', function() {
   it('should load chukfile configuration if it\'s a function', function() {
-    repl = chuk(null, function(context) {
+    var repl = chuk(null, function(context) {
       context.foo = 'bar';
     });
     repl.context.global.should.have.property('foo');
     repl.context.global.foo.should.equal('bar');
   });
   it('should load chukfile configuration for given env', function() {
-    repl = chuk('foo', config);
+    var repl = chuk('foo', config);
     repl.context.global.should.have.property('foo');
     repl.context.global.should.not.have.property('bar');
     repl.context.global.foo.should.equal('baz');
@@ -30,7 +30,7 @@ describe('chuk', function() {
     repl.context.global.bar.should.equal(123);
   });
   it('should load default configuration if no env given', function() {
-    repl = chuk(null, config);
+    var repl = chuk(null, config);
     repl.context.global.should.have.property('foo');
     repl.context.global.should.not.have.property('bar');
     repl.context.global.foo.should.equal('default');
