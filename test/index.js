30d647e5c8792f82d1972bbd7b8db9e4f592da6d
Stephan Hoyer <ste.hoyer@gmail.com>
2012-12-18 11:05:26 +0100

    Make scope of repl object in tests global again

--- test/index.js
+++ test/index.js
@@ -1,4 +1,5 @@
 var chuk = require('../')
+  , repl
   , config = {
   'foo': function(context) {
     context.foo = 'baz';
@@ -13,24 +14,24 @@ var chuk = require('../')
 
 describe('chuk', function() {
   it('should load chukfile configuration if it\'s a function', function() {
-    var repl = chuk(null, function(context) {
+    repl = chuk(null, function(context) {
       context.foo = 'bar';
     });
     repl.context.global.should.have.property('foo');
     repl.context.global.foo.should.equal('bar');
   });
   it('should load chukfile configuration for given env', function() {
-    var repl1 = chuk('foo', config);
-    repl1.context.global.should.have.property('foo');
-    repl1.context.global.should.not.have.property('bar');
-    repl1.context.global.foo.should.equal('baz');
-    var repl2 = chuk('bar', config);
-    repl2.context.global.should.not.have.property('foo');
-    repl2.context.global.should.have.property('bar');
-    repl2.context.global.bar.should.equal(123);
+    repl = chuk('foo', config);
+    repl.context.global.should.have.property('foo');
+    repl.context.global.should.not.have.property('bar');
+    repl.context.global.foo.should.equal('baz');
+    repl = chuk('bar', config);
+    repl.context.global.should.not.have.property('foo');
+    repl.context.global.should.have.property('bar');
+    repl.context.global.bar.should.equal(123);
   });
   it('should load default configuration if no env given', function() {
-    var repl = chuk(null, config);
+    repl = chuk(null, config);
     repl.context.global.should.have.property('foo');
     repl.context.global.should.not.have.property('bar');
     repl.context.global.foo.should.equal('default');
